4161a0370ec0c9c8206000dd020b787cfa35c72a
jeremy-gregoire <jeremy.gregoire@laplateforme.io>
2024-04-02 16:44:43 +0200

    Ajout de la partie javascript du job02

--- jour06/job02/script.js
+++ jour06/job02/script.js
@@ -1,5 +1,7 @@
 const elemPCitation = document.querySelector('#pCitation');
 const elemBtnRebootWorld = document.querySelector('#btnRebootWorld');
+const elemsPaginationLink = document.querySelectorAll('.pagination-link');
+const elemsListGroupItem = document.querySelectorAll('.list-group-item');
 
 const CITATIONS = [
   "J'ai vu tant de choses, que vous, humains, ne pourriez pas croire... De grands navires en feu surgissant de l'épaule d'Orion, j'ai vu des rayons fabuleux, des rayons C, briller dans l'ombre de la Porte de Tannhaüser. Tous ces moments se perdront dans l'oubli, comme les larmes dans la pluie. Il est temps de mourir.",
@@ -7,6 +9,12 @@ const CITATIONS = [
   "Tous ces moments se perdront dans l'oubli, comme des larmes dans la pluie.",
   'Avez-vous déjà désactivé un humain par erreur ?',
   "C'est dommage qu'elle doive mourir, mais on en est tous là !",
+];
+
+const MES_CITATIONS = [
+  'SAUSSURE !!!',
+  "Le désert c'est comme une plage mais sans la mer !",
+  "« Je fais le rêve qu'un jour cette nation se lèvera et vivra le vrai sens de sa foi : « Nous tenons ces vérités comme allant de soi, que les hommes naissent égaux. » » - Martin Lurther King",
 ];
 
 if (elemBtnRebootWorld) {
@@ -24,3 +32,30 @@ if (elemBtnRebootWorld) {
 } else {
   console.error("'elemBtnRebootWorld' is NULL!");
 }
+
+elemsPaginationLink.forEach((elemLink) => {
+  elemLink.addEventListener('click', (e) => {
+    e.preventDefault();
+    elemPCitation.textContent =
+      MES_CITATIONS[parseInt(e.target.textContent) - 1];
+  });
+});
+
+elemsListGroupItem.forEach((elemItem) => {
+  elemItem.style.cursor = 'pointer';
+  elemItem.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    let actives = [];
+    elemsListGroupItem.forEach((elem) => {
+      if (elem.classList.contains('active')) {
+        actives.push(elem);
+      }
+    });
+
+    if (actives !== 0) {
+      e.target.classList.add('active');
+      actives[0].classList.remove('active');
+    }
+  });
+});
